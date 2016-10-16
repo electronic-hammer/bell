22cb4d9472e868dd096248b21ba6f1c8afca13c9
Sergey Novikov <novikov@inventos.ru>
2016-10-16 23:47:21 +0700

    lesson/break

--- sketch_timer.ino
+++ sketch_timer.ino
@@ -1,36 +1,106 @@
+#include <LiquidCrystal.h>
+#include <TimerOne.h>
+
+LiquidCrystal lcd(12, 11, 10, 9, 8, 7);
+
+volatile unsigned int timer = 0;
+const unsigned int lessonDuration = 45;
+const unsigned int breakDuration = 10;
+volatile byte row = 0;
+const byte firstRow = 0;
+const byte secondRow = 1;
 const byte buttonLessonPin = 2;
 const byte buttonBreakPin = 3;
+volatile boolean timerStarted = false;
+const byte alarmDuration = 5;
 const byte jinglePin = 13;
-// in ms
-const unsigned long lessonDuration = 10000;
-const unsigned long breakDuration = 5000;
-volatile long timer = 0;
-const unsigned int tick = 1000;
-const unsigned int jingleDuration = 1000;
+
+void initializeTimer() {
+  Timer1.initialize();
+  Timer1.attachInterrupt(tick);
+  Timer1.stop();
+}
+
+void initializeDisplay() {
+  lcd.begin(16, 2);
+  printTimer(lessonDuration, firstRow);
+  printTimer(breakDuration, secondRow);
+}
+
+void initializeButtons() {
+  attachInterrupt(digitalPinToInterrupt(buttonBreakPin), startBreakTimer, HIGH);
+  attachInterrupt(digitalPinToInterrupt(buttonLessonPin), startLessonTimer, HIGH);
+}
+
+void initializeJingle() {
+  pinMode(jinglePin, OUTPUT);
+}
 
 void setup() {
-  pinMode(buttonLessonPin, INPUT);
-  pinMode(buttonBreakPin, INPUT);
-  pinMode(jinglePin, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(buttonLessonPin), startLessonTimer, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(buttonBreakPin), startBreakTimer, CHANGE);
+  initializeTimer();
+  initializeDisplay();
+  initializeButtons();
+  initializeJingle();
 }
 
-void loop() {
+void tick() {
   if (timer > 0) {
-    timer -= tick;
-    if (timer <= 0) {
-      tone(jinglePin, 1000, jingleDuration);
+    timer -= 1;
+    if (timer == 0) {
+      Timer1.stop();
     }
   }
-  delay(tick);
 }
 
-void startLessonTimer() {
-  timer = lessonDuration;
+void printTimer(unsigned int t, byte r) {
+  lcd.setCursor(5, r);
+  lcd.print(timerToString(t));
+}
+
+char* timerToString(unsigned int t) {
+  char res[6];
+  sprintf(res, "%02u:%02u", timer / 60, t % 60);
+  return res;
 }
 
 void startBreakTimer() {
-  timer = breakDuration;
+  startTimer(breakDuration + 1, secondRow);
+}
+
+void startLessonTimer() {
+  startTimer(lessonDuration + 1, firstRow);
+}
+
+void startTimer(unsigned int duration, byte r) {
+  if (!timerStarted) {
+    timer = duration;
+    row = r;
+    timerStarted = true;
+    lcd.clear();
+    Timer1.start();
+  }
+}
+
+void alarm() {
+  for (byte i = 0; i < alarmDuration; i++) {
+    printTimer(timer, row);
+    tone(jinglePin, 1000);
+    delay(500);
+    lcd.clear();
+    noTone(jinglePin);
+    delay(500);
+  }
+  timerStarted = false;
+  initializeDisplay();
+}
+
+void loop() {
+  if (timerStarted) {
+    printTimer(timer, row);
+    if (timer == 0) {
+      alarm();
+    }
+  }
+  delay(100);
 }
 
